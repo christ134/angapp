87263845006c5e681d76243d387eed07e6430c58
christ134 <48901442+christ134@users.noreply.github.com>
2019-04-25 16:47:38 +0530

    Add files via upload

---
+++ src/app/components/proadd/proadd.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proadd',
+  templateUrl: './proadd.component.html',
+  styleUrls: ['./proadd.component.css']
+})
+export class ProaddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
